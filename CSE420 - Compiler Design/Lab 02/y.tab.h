c2ef7b07880f060addbdf39a14c59c45b8e3970b
Mollah Md Saif <mollahmdsaif@gmail.com>
2024-07-07 21:13:01 +0600

    feat: add lab init

---
+++ CSE420 - Compiler Design/Lab 02/y.tab.h
@@ -0,0 +1,152 @@
+/* A Bison parser, made by GNU Bison 2.7.  */
+
+/* Bison interface for Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989-1990, 2000-2012 Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Enabling traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 1
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     CONTINUE = 258,
+     CHAR = 259,
+     DOUBLE = 260,
+     SWITCH = 261,
+     CASE = 262,
+     DEFAULT = 263,
+     IF = 264,
+     ELSE = 265,
+     FOR = 266,
+     WHILE = 267,
+     DO = 268,
+     BREAK = 269,
+     INT = 270,
+     FLOAT = 271,
+     VOID = 272,
+     RETURN = 273,
+     PRINTLN = 274,
+     ADDOP = 275,
+     MULOP = 276,
+     INCOP = 277,
+     DECOP = 278,
+     RELOP = 279,
+     ASSIGNOP = 280,
+     LOGICOP = 281,
+     NOT = 282,
+     LPAREN = 283,
+     RPAREN = 284,
+     LCURL = 285,
+     RCURL = 286,
+     LTHIRD = 287,
+     RTHIRD = 288,
+     COMMA = 289,
+     SEMICOLON = 290,
+     ID = 291,
+     CONST_INT = 292,
+     CONST_FLOAT = 293,
+     LOWER_THAN_ELSE = 294
+   };
+#endif
+/* Tokens.  */
+#define CONTINUE 258
+#define CHAR 259
+#define DOUBLE 260
+#define SWITCH 261
+#define CASE 262
+#define DEFAULT 263
+#define IF 264
+#define ELSE 265
+#define FOR 266
+#define WHILE 267
+#define DO 268
+#define BREAK 269
+#define INT 270
+#define FLOAT 271
+#define VOID 272
+#define RETURN 273
+#define PRINTLN 274
+#define ADDOP 275
+#define MULOP 276
+#define INCOP 277
+#define DECOP 278
+#define RELOP 279
+#define ASSIGNOP 280
+#define LOGICOP 281
+#define NOT 282
+#define LPAREN 283
+#define RPAREN 284
+#define LCURL 285
+#define RCURL 286
+#define LTHIRD 287
+#define RTHIRD 288
+#define COMMA 289
+#define SEMICOLON 290
+#define ID 291
+#define CONST_INT 292
+#define CONST_FLOAT 293
+#define LOWER_THAN_ELSE 294
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+#ifdef YYPARSE_PARAM
+#if defined __STDC__ || defined __cplusplus
+int yyparse (void *YYPARSE_PARAM);
+#else
+int yyparse ();
+#endif
+#else /* ! YYPARSE_PARAM */
+#if defined __STDC__ || defined __cplusplus
+int yyparse (void);
+#else
+int yyparse ();
+#endif
+#endif /* ! YYPARSE_PARAM */
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
